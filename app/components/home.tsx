18ab17ab3b4668005a68491e83bae09807f09271
aldreamlau <aldream.lau@gmail.com>
2023-04-18 09:47:10 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -9,8 +9,8 @@ import styles from "./home.module.scss";
 
 import SettingsIcon from "../icons/settings.svg";
 import GithubIcon from "../icons/github.svg";
-// import ChatGptIcon from "../icons/chatgpt.svg";
-import ChatGptIcon from "../icons/hani.svg";
+import ChatGptIcon from "../icons/chatgpt.svg";
+// import ChatGptIcon from "../icons/hani.svg";
 
 import BotIcon from "../icons/bot.svg";
 import AddIcon from "../icons/add.svg";
